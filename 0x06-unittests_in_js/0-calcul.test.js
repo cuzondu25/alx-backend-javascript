491fd69f9b5f30004cc14cc84d003259f408811d
cuzondu25 <cuzondu25@gmail.com>
2024-11-28 14:36:46 +0100

    Unit Test in JS

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,16 @@
+// Basic test with Mocha and Node assertion library
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('should return sum of two numbers rounded', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(2.4, 2.6), 5);
+    assert.strictEqual(calculateNumber(2.5, 3.4), 6);
+    assert.strictEqual(calculateNumber(-1.4, 0.4), -1);
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+});
---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,30 @@
+// Stubs
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberStub;
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('should verify that calculateNumber was called once with the correct arguments', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    // verify that calculateNumber was called once with the correct arguments
+    sinon.assert.calledOnce(calculateNumberStub);
+    sinon.assert.calledOnceWithExactly(calculateNumberStub, 'SUM', 100, 20);
+
+    // Verify that console.log was called with the correct message
+    sinon.assert.calledOnce(consoleLogSpy);
+    sinon.assert.calledOnceWithExactly(consoleLogSpy, 'The total is: 10');
+  });
+});
